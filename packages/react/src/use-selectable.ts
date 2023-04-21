0d1ba75a04686b4c620c5cba7b3d27322cd2dece
bryn.shanahan <brynshanahan17@gmail.com>
2023-04-21 16:20:33 +1000

    update

--- packages/react/src/use-selectable.ts
+++ packages/react/src/use-selectable.ts
@@ -1,15 +1,7 @@
-import { useEffect, useLayoutEffect, useRef, useState } from "react"
-import { strictEqual, Callback, SelectableInterface } from "@selkt/core"
-
-const isoLayoutEffect = // @ts-ignore
-  typeof window === "undefined" ? () => {} : useLayoutEffect
+import { useSyncExternalStore, useLayoutEffect, useRef, useState } from "react"
+import { strictEqual, SelectableInterface } from "@selkt/core"
 
 const ret: <T>(arg: T) => T = (v) => v
-type DeepRequired<T> = T extends object
-  ? Required<{
-      [K in keyof T]: NonNullable<DeepRequired<T[K]>>
-    }>
-  : T
 export function useSelectable<TState, TSlice = TState>(
   store: SelectableInterface<TState>,
   selector?: (arg: TState) => TSlice,
@@ -26,35 +18,17 @@ export function useSelectable<TState, TSlice = TState>(
   equalityCheck = strictEqual
 ): TSlice | undefined {
   let sel = selector ?? (ret as (arg: TState) => TSlice)
-  let [state, set] = useState(store ? () => sel(store.state) : undefined)
-  let previousRef = useRef(state)
 
-  isoLayoutEffect(() => {
-    let current = store ? sel(store.state) : undefined
-    if (!equalityCheck(current, previousRef.current)) {
-      previousRef.current = current
-      set(current)
-    }
-  }, [store])
-  useEffect(() => {
-    let current = store ? sel(store.state) : undefined
-    if (!equalityCheck(current, previousRef.current)) {
-      previousRef.current = current
-      set(current)
-    }
-    if (store) {
-      return store.select(
-        sel,
-        (slice) => {
-          if (!equalityCheck(slice, previousRef.current)) {
-            previousRef.current = slice
-            set(slice)
-          }
-        },
-        equalityCheck
-      )
-    }
-  })
-
-  return state
+  return useSyncExternalStore(
+    (onChange) => {
+      if (store) {
+        return store.select(sel, () => onChange(), equalityCheck)
+      } else {
+        onChange()
+        return () => {}
+      }
+    },
+    () => (store ? sel(store.state) : undefined),
+    () => (store ? sel(store.state) : undefined)
+  )
 }
